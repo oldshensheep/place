caf582aa8e2bc1e1f93847f44e4e27d034d0a232
oldshensheep <29306733+oldshensheep@users.noreply.github.com>
2023-01-15 12:53:25 +0800

    set `io.lettuce.core.jfr` to false

--- src/main/java/com/oldshensheep/place/PlaceApplication.java
+++ src/main/java/com/oldshensheep/place/PlaceApplication.java
@@ -11,6 +11,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class PlaceApplication {
 
     public static void main(String[] args) {
+        System.setProperty("io.lettuce.core.jfr", "false");
         SpringApplication.run(PlaceApplication.class, args);
     }
 
